99cebb3c3441e10018f0c325ba131e853fd9ef24
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-02-18 16:10:09 -0600

    Enviar imagenes al servidor y guardando su nombre

--- app/Http/Controllers/ImagenController.php
+++ app/Http/Controllers/ImagenController.php
@@ -2,13 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Imagen;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ImagenController extends Controller
 {
     public function store(Request $request)
     {
-        return $request->all();
-        return $request->file('file');
+
+        // leer la imagen
+        $ruta_imagen = $request->file('file')->store('establecimientos', 'public');
+        // rezise a la imagen
+        $imagen = Image::make(public_path("storage/{$ruta_imagen}"))->fit(800, 400);
+        $imagen->save();
+
+        // almacenar con Modelo
+        $imagenDB = new Imagen;
+        $imagenDB->id_establecimiento = $request['uuid'];
+        $imagenDB->ruta_imagen = $ruta_imagen;
+
+        $imagenDB->save();
+
+        $resp = [
+            'archivo' => $ruta_imagen,
+        ];
+
+        return response()->json($resp);
     }
 }
