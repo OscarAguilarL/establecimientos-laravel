75a0be6f63e7a408478c10a36ddfe5ae16317d1a
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-02-20 18:01:01 -0600

    Consumiendo los cafes y monstrandolos en la pagina principal

--- resources/js/components/CategoriaCafe.vue
+++ resources/js/components/CategoriaCafe.vue
@@ -1,7 +1,49 @@
 <template>
-  <h1>Desde cafe</h1>
+  <div class="container my-5">
+    <h2>Cafés</h2>
+
+    <div class="row">
+      <div
+        class="col-md-4 mt-4"
+        v-for="cafe in this.cafes"
+        v-bind:key="cafe.id"
+      >
+        <div class="card">
+          <img
+            :src="`storage/${cafe.imagen_principal}`"
+            :alt="cafe.nombre"
+            :title="cafe.nombre"
+            class="card-img-top"
+          />
+          <div class="card-body">
+            <h3 class="card-title text-primary font-weight-bold">{{ cafe.nombre }}</h3>
+            <p class="card-text">{{ cafe.direccion }}</p>
+            <p class="card-text">
+              <span class="font-weight-bold">Horario:</span>
+              {{ cafe.apertura }} - {{ cafe.cierre }}
+            </p>
+
+            <a href="" class="btn btn-primary d-block">Ver lugar</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-  export default {}
+  import axios from 'axios'
+
+  export default {
+    data: function () {
+      return {
+        cafes: [],
+      }
+    },
+    mounted() {
+      axios.get('/api/categorias/cafes').then((resp) => {
+        this.cafes = resp.data
+      })
+    },
+  }
 </script>
