0e954a931b684110a04a25aa9843ee8c46b89f97
Oscar Aguilar <aguilop.oscar@gmail.com>
2022-02-21 19:06:18 -0600

    Mostrando la informacion del establecimiento

--- resources/js/components/MostrarEstablecimiento.vue
+++ resources/js/components/MostrarEstablecimiento.vue
@@ -1,6 +1,40 @@
 <template>
   <div class="container my-5">
-    <h2>{{ establecimiento.nombre }}</h2>
+    <h2 class="text-center mb-5">{{ establecimiento.nombre }}</h2>
+    <div class="row align-items-start">
+      <div class="col-md-8">
+        <img
+          :src="`../storage/${establecimiento.imagen_principal}`"
+          :alt="establecimiento.nombre"
+          class="img-fluid"
+        />
+        <p class="mt-5">{{ establecimiento.descripcion }}</p>
+      </div>
+      <aside class="col-md-4 bg-primary">
+        <div></div>
+
+        <div class="p-4">
+          <h2 class="text-center text-white mt-2 mb-4">Más información</h2>
+
+          <p class="text-white mt-1">
+            <span class="font-weight-bold">Ubicación:</span>
+            {{ establecimiento.direccion }}
+          </p>
+          <p class="text-white mt-1">
+            <span class="font-weight-bold">Colonia:</span>
+            {{ establecimiento.colonia }}
+          </p>
+          <p class="text-white mt-1">
+            <span class="font-weight-bold">Horario:</span>
+            {{ establecimiento.apertura }} - {{ establecimiento.cierre }}
+          </p>
+          <p class="text-white mt-1">
+            <span class="font-weight-bold">Teléfono:</span>
+            {{ establecimiento.telefono }}
+          </p>
+        </div>
+      </aside>
+    </div>
   </div>
 </template>
 
